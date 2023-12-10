0b0d955e23bcd2eab4d53d887615d806fe53f9ce
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-10 11:06:05 +0100

    pom.xml: Syntax-Fehler gefixt

--- src/main/java/de/eldecker/dhbw/bruchrechnen/BruchDemo.java
+++ src/main/java/de/eldecker/dhbw/bruchrechnen/BruchDemo.java
@@ -5,13 +5,14 @@ package de.eldecker.dhbw.bruchrechnen;
  */
 public class BruchDemo {
 
-
     /**
      * Einstiegsmethode.
      *
      * @param args wird nicht verwendet
      */
     public static void main(String[] args) {
+
+            System.out.println("");
 
             Bruch bruch1 = new Bruch(1, 2);
 
@@ -24,5 +25,7 @@ public class BruchDemo {
             Bruch bruch3 = new Bruch(2, 3);
             bruch1.multiplizieren(bruch3);
             System.out.println(bruch1.getZaehler() + "/" + bruch1.getNenner());
+
+            System.out.println("");
     }
 }
