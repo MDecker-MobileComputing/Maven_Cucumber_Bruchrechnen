e69169a54674e224c2d7b299b38a879e04675a8e
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-10 10:50:45 +0100

    Dazu: maven_execute.sh

---
+++ maven_execute.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+
+./mvnw clean compile exec:java -Dexec.mainClass=de.eldecker.dhbw.bruchrechnen.BruchDemo
+
+echo
+
--- src/main/java/de/eldecker/dhbw/bruchrechnen/BruchDemo.java
+++ src/main/java/de/eldecker/dhbw/bruchrechnen/BruchDemo.java
@@ -4,24 +4,25 @@ package de.eldecker.dhbw.bruchrechnen;
  * Klasse mit main-Methode, um die Bruch-Klasse zu testen.
  */
 public class BruchDemo {
-    
+
+
     /**
      * Einstiegsmethode.
-     * 
+     *
      * @param args wird nicht verwendet
      */
     public static void main(String[] args) {
-            
+
             Bruch bruch1 = new Bruch(1, 2);
 
             // addiere 1/3 zu 1/2
-            Bruch bruch2 = new Bruch(1, 3);            
+            Bruch bruch2 = new Bruch(1, 3);
             bruch1.addiere(bruch2);
             System.out.println(bruch1.getZaehler() + "/" + bruch1.getNenner());
 
             // multipliziere mit 2/3
             Bruch bruch3 = new Bruch(2, 3);
-            bruch1.multiplizieren(bruch3);              
+            bruch1.multiplizieren(bruch3);
             System.out.println(bruch1.getZaehler() + "/" + bruch1.getNenner());
     }
 }
